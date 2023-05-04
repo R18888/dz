bb595f23bc84695e504b7e76889119b8f526aa4a
R18888 <Roman.Prokopev2012@yandex.ru>
2023-05-04 13:16:41 +0300

    added output of the first n fibonacci numbers

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,16 @@
-#include <iostream> 
-#include <bits/stdc++.n> 
-using namespace std; 
+#include <iostream>
+#include <bits/stdc++.n>
+ using namespace std;
 int f(int n) {
-	if ( n == 1 || n == 2) return (n -1);
+    if (n == 1 || n == 2) return (n-1);
     return f(n-1) + f(n-2);
 }
 int main() {
-    cout << "Hello, World!";
     cout << "Hello, World!\n";
     int n;
     cout << "n="; cin >> n;
-    cout << f(n) << "\n";
-    return 0;
+    for (int i = 1; i <= n; i++) {
+        cout << f(i) << endl;
     }
+    return 0;
+}
