44fcb7b9544018904429e296071ac130c835f9d8
R18888 <Roman.Prokopev2012@yandex.ru>
2023-05-04 12:56:58 +0300

    adding a recursive function

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,15 @@
- cout << "Hello, World!";
+#include <iostream> 
+#include <bits/stdc++.n> 
+using namespace std; 
+int f(int n) {
+	if ( n == 1 || n == 2) return (n -1);
+    return f(n-1) + f(n-2);
+}
+int main() {
+    cout << "Hello, World!";
     cout << "Hello, World!\n";
     int n;
     cout << "n="; cin >> n;
     cout << f(n) << "\n";
     return 0;
     }
-
